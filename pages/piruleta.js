a61edc1868e33a033563dbb70bf29fc0d1a4f72b
d3vv3 <jaimecondel@gmail.com>
2022-02-13 13:36:58 +0100

    fix: typo

--- pages/piruleta.js
+++ pages/piruleta.js
@@ -4,7 +4,7 @@ function Piruleta() {
   return <div className="piruletas-result-container">
     <Piruletas />
     <div className="message-container">
-        <h2>¡Hay priuleta/s para tí!</h2>
+        <h2>¡Hay piruleta/s para tí!</h2>
         <p>¿Cuántas? ª</p>
         <p>Las repartiremos el lunes y martes por las clases. Si por algún motivo no te pillamos en clase, puedes ir a recogerla/s a Eurielec (208-L). ¡Recuerda llevar identificación!</p>
     </div>
